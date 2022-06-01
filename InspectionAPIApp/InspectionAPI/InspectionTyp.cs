2203f99abf77fc9002be09edf292bf63f1f20875
Markus Staub <markusstaub1@gmail.com>
2022-06-01 18:33:11 +0200

    Table Inspection, Table InspectionTyp erstellt

---
+++ InspectionAPIApp/InspectionAPI/InspectionTyp.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class InspectionTyp
+    {
+        public int id { get; set; }
+
+        [StringLength(20)]
+        public string InspectionName { get; set; } = string.Empty;
+
+    }
+}
